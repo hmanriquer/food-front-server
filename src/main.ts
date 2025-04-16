503383c75e3a31318645220d19277611bfa6c77a
Hamed Manrique <manrique.hamed1610@gmail.com>
2025-04-16 16:35:37 -0600

    feat: add Swagger documentation and update dependencies
    - Integrated Swagger for API documentation with versioning support.
    - Updated package dependencies including @nestjs/swagger and swagger-ui-express.
    - Removed unused methods and cleaned up the AppController and AppService.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  ClassSerializerInterceptor,
+  ValidationPipe,
+  VersioningType,
+} from '@nestjs/common';
+import { PrismaClientExceptionFilter } from 'nestjs-prisma';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+
+  // Global configurations
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+    prefix: 'v',
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Food Front API Documentation')
+    .setDescription('API documentation for the Food Front application')
+    .setVersion('1.0')
+    .addTag('food-front')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
